60b994eba42c56eaca22fe3b7421bd66470e8c58
arjunjanamatti <arjunjanamatti@gmail.com>
2021-06-02 23:32:22 +0530

    first commit

--- Chapter_2_scripts/distributions.py
+++ Chapter_2_scripts/distributions.py
@@ -10,6 +10,16 @@ from glob import glob
 
 filename = "C:/Users/Arjun Janamatti/PycharmProjects/thinkstats_book/Data/preg_df.pickle"
 
+with open(filename, 'rb') as read_file:
+    data = pickle.load(read_file)
 
+print(data['prglength'].value_counts().index)
+print(dict(data['prglength'].value_counts()))
+print(list(data['prglength'].value_counts()))
 
 hist = {}
+for x in data['prglength']:
+    hist[x] = hist.get(x, 0) + 1
+
+print(hist)
+
