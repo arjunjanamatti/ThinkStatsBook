e84b943f1501f4ec1dbc25899e97a99e0bd07c1a
arjunjanamatti <arjunjanamatti@gmail.com>
2021-06-02 23:09:15 +0530

    first commit

---
+++ Chapter_2_scripts/distributions.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+# Histogram shows the frequency of each value. Here frequency is the number of times each value appears.
+import pickle
+from glob import glob
+
+filename = "C:/Users/Arjun Janamatti/PycharmProjects/thinkstats_book/Data/preg_df.pickle"
+
+
+
+hist = {}
