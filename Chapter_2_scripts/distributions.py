6c5475e03cb09f58e301739ede59c4ae0ac416b8
arjunjanamatti <arjunjanamatti@gmail.com>
2021-06-03 19:46:54 +0530

    removal of print statements

--- Chapter_2_scripts/distributions.py
+++ Chapter_2_scripts/distributions.py
@@ -19,19 +19,5 @@ data = data[data['prglength'] < 51]
 # Histogram for the pregnancy length
 hist_prglength = dict(data['prglength'].value_counts().sort_index())
 
-print(hist_prglength)
-print(min(hist_prglength.values()), max(hist_prglength.values()))
-print(min(data['prglength']), max(data['prglength']))
 plt.bar(hist_prglength.keys(), hist_prglength.values())
 plt.show()
-
-
-# check for grouping
-date_list = ['25-01-2020', '25-01-2020', '26-01-2020']
-name_list = ['A', 'A', 'A']
-check_df = pd.DataFrame()
-check_df['name_list'] = name_list
-check_df['date_list'] = date_list
-check_df['date_list'] = pd.to_datetime(check_df['date_list'],dayfirst=True)
-
-print(check_df.groupby(by=['name_list'])['date_list'].count())
