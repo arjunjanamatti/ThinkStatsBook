22f78e646cc8d7336ac159dc3dc97ea6fcf78e12
arjunjanamatti <arjunjanamatti@gmail.com>
2021-06-06 11:49:06 +0530

    first commit

--- learn_async/1.py
+++ learn_async/1.py
@@ -1,18 +1,23 @@
 import asyncio
 import time
 
-async def count():
-    print('One')
-    await asyncio.sleep(0.00000001)
-    print('Two')
+def square_num(num):
+    return num**2
 
-async def main():
-    await asyncio.gather(count(), count(), count())
+async def count(num):
+    print(f'Square of {num}: {square_num(num)}')
+    await asyncio.sleep(0.00000001)
+    print(f'Square of {num+5}: {square_num(num+5)}')
+
+
+async def main(num):
+    await asyncio.gather(count(num), count(num), count(num))
 
 
 if __name__ == '__main__':
     start_time = time.perf_counter()
-    asyncio.run(main())
+    num = 10
+    asyncio.run(main(num))
     end_time = time.perf_counter()
     total_time = end_time - start_time
     print(f'Total time: {total_time}')
